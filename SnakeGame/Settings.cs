25bef47a8f39b7178015df7129bbb09236c6c917
zdzulak <43354865+zdzulak@users.noreply.github.com>
2019-04-18 10:43:28 -0400

    final

---
+++ SnakeGame/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+
+    public enum Directions
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    };
+    class Settings
+    {
+        
+
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            Speed = 20;
+            Score = 0;
+            Points = 100;
+            GameOver = false;
+            direction = Directions.Down;
+        }
+
+        public static int Width { get; set; }
+            public static int Height { get; set; }
+            public static int Speed { get; set; }
+            public static int Score { get; set; }
+            public static int Points { get; set; }
+            public static bool GameOver { get; set; }
+            public static Directions direction { get; set; }
+
+            
+   
+    }
+}
